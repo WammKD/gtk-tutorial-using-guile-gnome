d490e46fa6bd99aa83d106a86a842fb3d4f05ab5
Sherlock Holmes <jaft.r@outlook.com>
2019-12-07 06:33:44 -0600

    Good Hell, this one was a pain to figure out…
    Mostly it was the enums/constants; I put comments for where I finally
    figured out what was supposed to be what but the fact that these
    aren't already defined, in some way (or maybe they are; but Hell if I
    could find the documentation!), is disheartening.
    
    I'm counting on the keyval to sort of be able to be pieced back
    together from the function that's used (it just amounts to the number
    113) but I was able to get there from these Ruby doc.s:
    https://ruby-gnome2.osdn.jp/de/hiki.cgi?Gdk%3A%3AKeyval#GDK_Q.
    
    Also, the documentation says you can pass `#f` for
    `gtk-image-menu-item-new-from-stock` to not associate an `accel-group`
    but that didn't work so we improvised.
    
    Otherwise, about as straightforward as the previous examples.

---
+++ menus_toolbars/images_mnemonics_accelerators.scm
@@ -0,0 +1,40 @@
+(use-modules (gnome-2) (oop goops) (gnome gobject) (gnome gtk))
+
+(define (images-mnemonics-accelerators)
+  (let* ([window     (gtk-window-new 'toplevel)]
+         [vbox       (gtk-vbox-new #f 0)]
+         [menubar    (gtk-menu-bar-new)]
+         [fileMenu   (gtk-menu-new)]
+         [fileMi     (gtk-menu-item-new-with-mnemonic "_File")]
+         [ newMi     (gtk-image-menu-item-new-from-stock (gtk-stock-id 'new)  (gtk-accel-group-new))]
+         [openMi     (gtk-image-menu-item-new-from-stock (gtk-stock-id 'open) (gtk-accel-group-new))]
+         [sep        (gtk-separator-menu-item-new)]
+         [accelGroup (gtk-accel-group-new)]
+         [quitMi     (gtk-image-menu-item-new-from-stock (gtk-stock-id 'quit) accelGroup)])
+    (set-title        window "Images")
+    (set-default-size window 300 200)
+    (set-position     window 'center)
+
+    (add window vbox)
+
+    (add-accel-group window accelGroup)
+
+    (gtk-widget-add-accelerator quitMi                     "activate" accelGroup
+                                (gdk-keyval-from-name "q") (ash 1 2)  1) ;https://lazka.github.io/pgi-docs/Gtk-3.0/flags.html#Gtk.AccelFlags
+                                                           ;https://code.woboq.org/gtk/gtk/gdk/gdktypes.h.html#GDK_CONTROL_MASK
+
+    (set-submenu fileMi fileMenu)
+    (gtk-menu-shell-append fileMenu newMi)
+    (gtk-menu-shell-append fileMenu openMi)
+    (gtk-menu-shell-append fileMenu sep)
+    (gtk-menu-shell-append fileMenu quitMi)
+    (gtk-menu-shell-append menubar fileMi)
+    (pack-start vbox menubar #f #f 0)
+
+    (connect window 'destroy  (lambda (w) (gtk-main-quit)))
+    (connect quitMi 'activate (lambda (w) (gtk-main-quit)))
+
+    (show-all window)))
+
+(images-mnemonics-accelerators)
+(gtk-main)
