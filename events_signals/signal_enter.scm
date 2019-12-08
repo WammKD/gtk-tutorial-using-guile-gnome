5bdd31d9e74aea98c6786570300a59412bb4f2db
Sherlock Holmes <jaft.r@outlook.com>
2019-12-08 05:03:06 -0600

    I do not get this damn library.
    The documentation very clearly states that the last argument ought to
    be a <gdk-color>; yet supplying `(make <gdk-color> #:value #(27000
    30000 35000))` causes it to balk about `vector-ref` getting the wrong
    type.
    
    So I dunno; no matter the value I set, it didn't change anything. But
    nothing errorred out.

---
+++ events_signals/signal_enter.scm
@@ -0,0 +1,32 @@
+(use-modules (gnome-2) (oop goops) (gnome gobject) (gnome gtk))
+
+(define (enter-button button)
+  (display "Ya hovered!")
+  (newline)
+
+  (modify-bg
+    button
+    (make <gtk-state-type> #:value 'prelight)
+    #(0 27000 30000 35000)))
+
+(define (enter)
+  (let ([window (gtk-window-new 'toplevel)]
+        [halign (gtk-alignment-new 0 0 0 0)]
+        [btn    (gtk-button-new-with-label "Button")])
+    (set-title        window "Enter signal")
+    (set-default-size window 300 200)
+    (set-position     window 'center)
+    (set-border-width window 15)
+
+    (set-size-request btn 70 30)
+
+    (add halign btn)
+    (add window halign)
+
+    (connect btn    'enter   enter-button)
+    (connect window 'destroy (lambda (w) (gtk-main-quit)))
+
+    (show-all window)))
+
+(enter)
+(gtk-main)
